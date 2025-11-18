274576b115886b94d877ab48435c9e1c707a8e80
RalphHellisng62 <arenasrafael889@gmail.com>
2025-11-18 17:35:30 -0600

    Iniciamos construyendo el main, pedira el archivo de entrada y mostrara el archivo de salida

--- RadixSort.java
+++ RadixSort.java
@@ -1,3 +1,30 @@
+
 public class RadixSort {
+    // Iniciamos construyendo un main
+
+    public static void main(String[] args) {
+        // Sera para llamar el archivo de entrada y de salida
+
+        String archivoEntrada = "numeros.txt";
+        String archivoSalida = "numeros_ordenados_radix.txt";
+
+        System.out.println("📥 Leyendo archivo: " + archivoEntrada);
+        int[] numeros = leerArchivo(archivoEntrada);
+
+        System.out.println("\n numeros originales:");
+        imprimirArray(numeros);
+
+        System.out.println("\n Se esta ejecutando RadixSort (se ordena en tiempo real):");
+        radixSort(numeros);
+
+        System.out.println("\n numeros ordenados:");
+        imprimirArray(numeros);
+
+        guardarArchivo(archivoSalida, numeros);
+        System.out.println("\n Archivo ordenado guardado como: " + archivoSalida);
+
+    }
+
     
+
 }
